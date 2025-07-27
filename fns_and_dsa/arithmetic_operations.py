9fe717afd5fd9556ff0aefcdf5c812f6ce24b8b3
MzAkorkor <mzakorkor@gmail.com>
2025-07-27 16:21:13 +0000

    Fixed missing operations and division by zero handling

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,8 +8,7 @@ def perform_operation(num1, num2, operation):
     elif operation == "divide":
         if num2 == 0:
             return "Error: Cannot divide by zero"
-        else:
-            return num1/num2
+        return num1 / num2
     else:
         return "Error: Invalid operation"
             
